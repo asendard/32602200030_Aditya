a7b3a194f29da88d0e5949bca2072ebeadc2e9ed
Aditya Sendy Ardiansyah <adityasendyardiansyah@gmail.com>
2024-06-12 14:28:37 +0700

    Fisrt Commit

---
+++ app/Controllers/HelloWorld.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class HelloWorld extends Controller
+{
+    public function halodunia()
+    {
+        return '<h1>Hello World</h1>';
+    }
+}
